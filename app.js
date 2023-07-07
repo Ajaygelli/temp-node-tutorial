ac6c75efce6296bfffb9a9fdd730bc6ab85989b9
Ajaygelli <saiajayg.skp.com>
2023-07-07 15:45:18 +0530

    aaa

--- app.js
+++ app.js
@@ -1,45 +1,10 @@
-const {readFile,writeFile}=require('fs');
-const { resolve } = require('path');
 
-//Now with promise function
-//ASYNCit add gi
- const getText= (path) =>{
+const Eventemitter= require('events');
+//Lets take the module i mean import it.
+const customEmitter= new Eventemitter()
 
-    return new Promise((resolve,reject)=>{
-        readFile(path,'utf-8',(err,data)=>{
-            if(err){
-                reject(err)
-            }
-            else
-            {
-                resolve(data);
-            }
-         })
+customEmitter.on('response',()=>{
+    console.log('data recieved');
+})
 
-    })
- }
-//  //Normal code for reading data from txt file
-//  const getdata= readFile(path,'utf-8',(err,data)=>{
-//     if(err){
-//         return;
-//     }
-//     else
-//     {
-//         console.log(data);
-//     }
-//  })
-
-//if our promise is resolved then we use".then" if failed i mean rejected we use ".catch"
-//so .then will handle resolved values where as .catch will handle errors
-
-getText('./content/final.txt')
-.then((result)=>console.log(result+'  '))
-.catch((err)=>console.log(err))
-
-// u may observe now that getText is an ASYNCHRONUS fun so the next piece of code get exectued 
-//then the call back function executed like adding in a callback queue
-console.log('sdafghjkl');
-
-getText('./content/second.txt')
-.then((result)=>console.log(result))
-.catch((err)=>console.log(err))
+customEmitter.emit('response')
